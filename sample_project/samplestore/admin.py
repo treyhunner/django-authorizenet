c80d0f847df9bb00e9e2ac80ff9e9314451371f7
Trey Hunner <treyhunner@gmail.com>
2011-03-15 12:15:01 -0700

    Add sample CIM customer models

--- sample_project/samplestore/admin.py
+++ sample_project/samplestore/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from samplestore.models import Invoice, Item, Customer, Address
+from samplestore.models import Invoice, Item, Customer, CustomerProfile, \
+CustomerPaymentProfile, Address
 
 admin.site.register(Invoice)
 admin.site.register(Item)
 admin.site.register(Customer)
+admin.site.register(CustomerProfile)
+admin.site.register(CustomerPaymentProfile)
 admin.site.register(Address)
--- sample_project/samplestore/models.py
+++ sample_project/samplestore/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 from django.contrib.localflavor.us.models import PhoneNumberField, USStateField
 
 from authorizenet.signals import payment_was_successful, payment_was_flagged
+from authorizenet.cim import add_profile, get_profile, \
+        update_payment_profile, create_payment_profile, delete_payment_profile
+from django.forms.models import model_to_dict
+from samplestore.errors import BillingError
 
 
 ADDRESS_CHOICES = (
@@ -19,6 +23,136 @@ class Customer(models.Model):
 
     def __unicode__(self):
         return self.user.username
+
+
+class CustomerProfileManager(models.Manager):
+    def create(self, **kwargs):
+        """Create new Authorize.NET customer profile"""
+        user = kwargs.get('user')
+        payment_data = kwargs.pop('payment_data', {})
+        billing_data = kwargs.pop('billing_data', {})
+        # Create the customer profile with Authorize.NET CIM call
+        response, profile_id, payment_profile_ids = add_profile(user.pk,
+                payment_data, billing_data)
+        if not response.success:
+            raise BillingError()
+        kwargs['profile_id'] = profile_id
+        # Store customer profile data locally
+        instance = super(CustomerProfileManager, self).create(**kwargs)
+
+        # Store customer payment profile data locally
+        for payment_profile_id in payment_profile_ids:
+            CustomerPaymentProfile.objects.create(
+                customer_profile=instance,
+                payment_profile_id=payment_profile_id,
+                billing_data=billing_data,
+                payment_data=payment_data,
+                make_cim_request=False,
+            )
+
+        return instance
+
+
+class CustomerProfile(models.Model):
+
+    """Authorize.NET customer profile"""
+
+    user = models.ForeignKey(User, unique=True)
+    profile_id = models.CharField(max_length=50)
+
+    def sync(self):
+        """Overwrite local customer profile data with remote data"""
+        response, payment_profiles = get_profile(self.profile_id)
+        if not response.success:
+            raise BillingError()
+        for payment_profile in payment_profiles:
+            instance, created = CustomerPaymentProfile.objects.get_or_create(
+                customer_profile=self,
+                payment_profile_id=payment_profile['payment_profile_id']
+            )
+            instance.sync(payment_profile)
+
+    objects = CustomerProfileManager()
+
+
+class CustomerPaymentProfileManager(models.Manager):
+    def create(self, **kwargs):
+        """Create new Authorize.NET customer payment profile"""
+        customer_profile = kwargs.get('customer_profile')
+        payment_data = kwargs.pop('payment_data', {})
+        billing_data = kwargs.pop('billing_data', {})
+        if kwargs.pop('make_cim_request', True):
+            # Create the customer payment profile with Authorize.NET CIM call
+            response, payment_profile_id = create_payment_profile(
+                    customer_profile.profile_id, payment_data, billing_data)
+            if not response.success:
+                raise BillingError()
+            kwargs['payment_profile_id'] = payment_profile_id
+        kwargs.update(billing_data)
+        kwargs.update(payment_data)
+        kwargs.pop('expiration_date')
+        kwargs.pop('card_code')
+        if 'card_number' in kwargs:
+            kwargs['card_number'] = "XXXX%s" % kwargs['card_number'][-4:]
+        return super(CustomerPaymentProfileManager, self).create(**kwargs)
+
+
+class CustomerPaymentProfile(models.Model):
+
+    """Authorize.NET customer payment profile"""
+
+    customer_profile = models.ForeignKey('CustomerProfile',
+            related_name='payment_profiles')
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    company = models.CharField(max_length=50, blank=True)
+    address = models.CharField(max_length=60, blank=True)
+    city = models.CharField(max_length=40, blank=True)
+    state = models.CharField(max_length=40, blank=True)
+    zip = models.CharField(max_length=20, blank=True, verbose_name="ZIP")
+    country = models.CharField(max_length=60, blank=True)
+    phone = models.CharField(max_length=25, blank=True)
+    fax = models.CharField(max_length=25, blank=True)
+    payment_profile_id = models.CharField(max_length=50)
+    card_number = models.CharField(max_length=16, blank=True)
+
+    def raw_data(self):
+        """Return data suitable for use in payment and billing forms"""
+        return model_to_dict(self)
+
+    def sync(self, data):
+        """Overwrite local customer payment profile data with remote data"""
+        for k, v in data.get('billing', {}).items():
+            setattr(self, k, v)
+        self.card_number = data.get('credit_card', {}).get('card_number',
+                self.card_number)
+        self.save()
+
+    def delete(self):
+        """Delete the customer payment profile remotely and locally"""
+        delete_payment_profile(self.customer_profile.profile_id,
+                self.payment_profile_id)
+
+    def update(self, payment_data, billing_data):
+        """Update the customer payment profile remotely and locally"""
+        response = update_payment_profile(self.customer_profile.profile_id,
+                self.payment_profile_id, payment_data, billing_data)
+        if not response.success:
+            raise BillingError()
+        for k, v in billing_data.items():
+            setattr(self, k, v)
+        for k, v in payment_data.items():
+            # Do not store expiration date and mask credit card number
+            if k != 'expiration_date' and k != 'card_code':
+                if k == 'card_number':
+                    v = "XXXX%s" % v[-4:]
+                setattr(self, k, v)
+        self.save()
+
+    def __unicode__(self):
+        return self.card_number
+
+    objects = CustomerPaymentProfileManager()
 
 
 class Address(models.Model):
